8cddf9d1212fe01c81de3ae99803bf13cd4b8276
stvikuru <Vishnudev.Kurumbaparambil@student.hs-anhalt.de>
2025-01-17 16:58:58 +0100

    new model | rviz is okay, gazebo not proper

--- Scotty_Champ/scotty_config/include/quadruped_description.h
+++ Scotty_Champ/scotty_config/include/quadruped_description.h
@@ -12,22 +12,22 @@ namespace champ
       base.lf.hip.setOrigin(0.2485, 0.065, 0.025, 0.0, 0.0, 0.0);
 base.lf.upper_leg.setOrigin(0.065, 0.03, -0.0, 0.0, 0.0, 0.0);
 base.lf.lower_leg.setOrigin(0.0, 0.087405, -0.3, 0.0, 0.0, 0.0);
-     base.lf.foot.setOrigin(0.0, 0.0, -0.32, 0.0, 0.0, 0.0);
+     base.lf.foot.setOrigin(0.0175, 0.004395, -0.3, 0.0, 0.0, 0.0);
 
       base.rf.hip.setOrigin(0.2485, -0.065, 0.025, 0.0, 0.0, 0.0);
 base.rf.upper_leg.setOrigin(0.065, -0.03, -0.0, 0.0, 0.0, 0.0);
 base.rf.lower_leg.setOrigin(0.0, -0.087405, -0.3, 0.0, 0.0, 0.0);
-     base.rf.foot.setOrigin(0.0, 0.0, -0.32, 0.0, 0.0, 0.0);
+     base.rf.foot.setOrigin(0.0175, 0.001605, -0.3, 0.0, 0.0, 0.0);
 
       base.lh.hip.setOrigin(-0.2485, 0.065, 0.025, 0.0, 0.0, 0.0);
 base.lh.upper_leg.setOrigin(-0.065, 0.03, -0.0, 0.0, 0.0, 0.0);
 base.lh.lower_leg.setOrigin(0.0, 0.087405, -0.3, 0.0, 0.0, 0.0);
-     base.lh.foot.setOrigin(0.0, 0.0, -0.32, 0.0, 0.0, 0.0);
+     base.lh.foot.setOrigin(0.0175, 0.004395, -0.3, 0.0, 0.0, 0.0);
 
       base.rh.hip.setOrigin(-0.2485, -0.065, 0.025, 0.0, 0.0, 0.0);
 base.rh.upper_leg.setOrigin(-0.065, -0.03, -0.0, 0.0, 0.0, 0.0);
 base.rh.lower_leg.setOrigin(0.0, -0.087405, -0.3, 0.0, 0.0, 0.0);
-     base.rh.foot.setOrigin(0.0, 0.0, -0.32, 0.0, 0.0, 0.0);
+     base.rh.foot.setOrigin(0.0175, 0.001605, -0.3, 0.0, 0.0, 0.0);
         }
     }
 }
