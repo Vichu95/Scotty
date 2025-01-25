1e9e10e86fdc5435bbc8c705e4ee06efad1f2919
stvikuru <Vishnudev.Kurumbaparambil@student.hs-anhalt.de>
2025-01-25 17:19:22 +0100

    Emergency stop added | Console log from html

---
+++ Scotty_Champ/scotty_config/scripts/emergency_stop.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+import rosnode
+import os
+import signal
+from std_msgs.msg import Bool
+import subprocess
+
+class EmergencyStop:
+    def __init__(self):
+        rospy.init_node('emergency_stop', anonymous=False)
+        rospy.loginfo("Emergency Stop Node Initialized")
+
+        # Subscriber to listen for emergency stop signal
+        self.emergency_sub = rospy.Subscriber('/scotty_controller/emergency_stop', Bool, self.handle_emergency)
+
+    def handle_emergency(self, msg):
+        """Callback for emergency stop signal."""
+        if msg.data:
+            rospy.logwarn("Emergency Stop Triggered! Killing all ROS nodes...")
+            self.kill_all_ros_nodes()
+        else:
+            rospy.loginfo("Emergency Stop signal received, but not activated.")
+
+    def kill_all_ros_nodes(self):
+        """Kills all active ROS nodes."""
+        try:
+            subprocess.Popen(["rosnode", "kill", "/scotty_main_controller"])
+            subprocess.call(["pkill", "-f", "gzserver"])
+            subprocess.call(["pkill", "-f", "gzclient"])
+            # List all active ROS nodes
+            nodes = rosnode.get_node_names()
+            for node in nodes:
+                rospy.loginfo("Killing node: {}".format(node))
+                os.system("rosnode kill {}".format(node))
+            rospy.loginfo("All ROS nodes terminated successfully.")
+        except Exception as e:
+            rospy.logerr("Failed to kill nodes: {}".format(e))
+
+if __name__ == "__main__":
+    try:
+        EmergencyStop()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
