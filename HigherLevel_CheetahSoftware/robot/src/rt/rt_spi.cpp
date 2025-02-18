75d791813d42a2513dae15f7f104f6a79d1af7dd
stvikuru <Vishnudev.Kurumbaparambil@student.hs-anhalt.de>
2025-02-18 13:04:18 +0100

    abad offset value added to MIT rt_spi | due to zero ing issue

--- HigherLevel_CheetahSoftware/robot/src/rt/rt_spi.cpp
+++ HigherLevel_CheetahSoftware/robot/src/rt/rt_spi.cpp
@@ -60,9 +60,9 @@ const float disabled_torque[3] = {0.f, 0.f, 0.f};
 
 
 
-const float abad_offset[4] = {-0.f, 0.f, 0.f, 0.f};
-const float hip_offset[4] = {-0.f, 0.f, 0.f, 0.f};
-const float knee_offset[4] = {-0.f, 0.f, 0.f, 0.f};
+const float abad_offset[4] = {0.42f, 0.f, 0.f, 0.f};
+const float hip_offset[4] = {0.f, 0.f, 0.f, 0.f};
+const float knee_offset[4] = {0.f, 0.f, 0.f, 0.f};
 
 const float abad_side_sign[4] = {-1.f, -1.f, 1.f, 1.f};
 const float hip_side_sign[4] = {-1.f, 1.f, -1.f, 1.f}; //reset to mit
