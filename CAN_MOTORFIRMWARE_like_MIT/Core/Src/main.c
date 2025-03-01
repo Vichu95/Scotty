b9db54b5d7bd2d53cbf2d0498127dda4d9d7ad0e
Vichu95 <k.vishnudev@gmail.com>
2025-03-01 21:37:39 +0100

    One macro for torque limit | Knee has same pos neg range

--- CAN_MOTORFIRMWARE_like_MIT/Core/Src/main.c
+++ CAN_MOTORFIRMWARE_like_MIT/Core/Src/main.c
@@ -41,15 +41,15 @@
 #define MOTOR_KD_MAX 5.0f
 
 // Max torque that can be requested from the motor
-#define TRQ_REQ_MAX 3.0f
+#define TRQ_REQ_MAX 1.5f
 
 // STM limits for P V T Kd Kp
 #define P_MIN -12.5f
 #define P_MAX  12.5f
 #define V_MIN -26.0f
 #define V_MAX  26.0f
-#define T_MIN -1.5f
-#define T_MAX  1.5f
+#define T_MIN -TRQ_REQ_MAX
+#define T_MAX  TRQ_REQ_MAX
 #define KP_MIN 0.0f
 #define KP_MAX 500.0f
 #define KD_MIN 0.0f
@@ -61,7 +61,7 @@
 #define HIP_LIM_P 	 2.0944f 	//120°
 #define HIP_LIM_N 	-2.0944f 	//120°
 #define KNEE_LIM_P	 4.01426f 		//-230°
-#define KNEE_LIM_N 	-0.1f	//5.7°
+#define KNEE_LIM_N 	-4.01426f  //-0.1f	//5.7° //todo proper limits, consider direction change
 #define KP_SOFTSTOP  100.0f
 #define KD_SOFTSTOP  0.4f
 
