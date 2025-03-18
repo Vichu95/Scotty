44c7c757ae39cd62eec6c7eced3837ce9b3d7a9e
stvikuru <Vishnudev.Kurumbaparambil@student.hs-anhalt.de>
2025-03-18 19:36:57 +0100

    Walk script > passes argument hardware connected or not. needed for champ

--- Scotty_Champ/scotty_controller/scripts/state_controllers/scotty_state_walk.py
+++ Scotty_Champ/scotty_controller/scripts/state_controllers/scotty_state_walk.py
@@ -24,10 +24,14 @@ class WalkController:
         # Publisher to broadcast logs to the GUI console
         self.console_log_pub = rospy.Publisher("/scotty_controller/console_log", String, queue_size=10)
 
-    def start_champ_controller(self):
+    def start_champ_controller(self,mode):
         rospy.loginfo("WalkController : Starting CHAMP controller via bringup.launch")
-        # Replace 'path_to_champ_controller.launch' with the correct path
-        subprocess.Popen(["roslaunch", "scotty_config", "bringup.launch"])
+
+        if mode =="hardware":
+            subprocess.Popen(["roslaunch", "scotty_config", "bringup.launch", "hardware_connected:=true"])
+        else:
+            subprocess.Popen(["roslaunch", "scotty_config", "bringup.launch"])
+
         rospy.loginfo("WalkController : CHAMP controller started. Ready for teleop.")
         self.console_log_pub.publish("INFO    : Use teleop for moving the robot")
 
@@ -35,6 +39,6 @@ if __name__ == '__main__':
     try:
         mode = sys.argv[1] if len(sys.argv) > 1 else "simulation" # Default mode simulation
         controller = WalkController()
-        controller.start_champ_controller()
+        controller.start_champ_controller(mode)
     except rospy.ROSInterruptException:
         pass
